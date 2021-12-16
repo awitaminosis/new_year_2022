67e6fe9828c6f7bbbbb0f1dded019bda8d232746
awitaminosis <awitaminosis@mail.ru>
2021-12-16 13:53:35 +0300

    added cubey. start of threejs cube display

--- app/main.py
+++ app/main.py
@@ -1,34 +1,29 @@
-from fastapi import FastAPI
 from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+import cubey as qb
+import cube_manipulations
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-
-@app.get("/easy", response_class=HTMLResponse)
-async def respond_tmp(request: Request):
-    return templates.TemplateResponse("easy.html", {"request": request})
-
-
-@app.get("/hard", response_class=HTMLResponse)
-async def respond_tmp(request: Request):
-    cube = []
-    return templates.TemplateResponse("hard.html", {"request": request, 'cube': cube})
+c = qb.Cube()
+c.r()
+c.u()
+c.r_prime()
+c.u_prime()
+print(cube_manipulations.cube_sides(c))
+print (cube_manipulations.print_cube(c))
 
 
 @app.get("/", response_class=HTMLResponse)
 async def respond_home(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+    return templates.TemplateResponse("home.html", {"request": request, "cube": c})
 
-
-# raise HTTPException(
-#     status_code=404, detail=f"Memopi with ID {n} not found"
-# )
 
 if __name__ == '__main__':
     import uvicorn

9ae483a39ed4d3f9bd31b544bf9815bf1671a0f9
awitaminosis <awitaminosis@mail.ru>
2021-12-19 08:32:54 +0300

    quick solution for import inside docker problem

--- app/main.py
+++ app/main.py
@@ -6,8 +6,44 @@ from starlette.responses import RedirectResponse
 import uuid
 import random
 
+
 import cubey as qb
-import cube_manipulations
+######--{
+import colorama
+def print_cube(c):
+    DIMENSION = 3
+    iter_dim = range(DIMENSION)
+    string = ''
+
+    # Stringify all dimensions of the cube plus 1 for the header string
+    for i in range(DIMENSION + 1):
+        # First print the head string
+        if i == 0:
+            space = ' ' * DIMENSION
+            first_space = ' ' * int((DIMENSION / 2))
+            # The below order is the same order we add in the init method
+            string += colorama.Fore.BLACK + first_space + 'F' + space + 'B' + space + 'L' + space + 'R' + space + 'U' + space + 'D\n'
+        # Next print each row of each face
+        else:
+            for face in [c._F, c._B, c._L, c._R, c._U, c._D]:
+                for j in iter_dim:
+                    string += qb.color_strings[face[i - 1][j]]
+                string += ' '
+            string += '\n'
+    string += colorama.Style.RESET_ALL
+    return string
+
+def cube_sides(c):
+    return {
+        '_B': c._B,
+        '_D': c._D,
+        '_F': c._F,
+        '_L': c._L,
+        '_R': c._R,
+        '_U': c._U,
+    }
+######--}
+
 
 app = FastAPI()
 
@@ -90,7 +126,7 @@ async def respond_home(request: Request):
     printable_history = 'No history yet' if history.get(cube_id, None) is None else [x[0] for x in history.get(cube_id, None)]
 
     return templates.TemplateResponse("home.html", {"request": request,
-                                                    "cube": cube_manipulations.cube_sides(c),
+                                                    "cube": cube_sides(c),
                                                     'cube_id': cube_id,
                                                     'history': printable_history
                                                     })
@@ -115,4 +151,4 @@ async def congratulate(request: Request):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=8001)
+    uvicorn.run(app, host='0.0.0.0', port=8002)
